e1df3af9df0b6adae25e27c0227e5dbbb8461694
cheevandos <appdata2101@protonmail.com>
2021-09-26 18:38:10 +0400

    Added lab 2 logic

--- SecondLab/Program.fs
+++ SecondLab/Program.fs
@@ -1,13 +1,39 @@
-﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+﻿open System.IO
+open System.Collections.Generic
+open System.Linq
 
-open System
+let cleanString (str: string): string =
+    str.Replace(",", System.String.Empty).Replace(".", System.String.Empty)
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+let getWordsFromString (str: string): List<string> =
+    str.Split(' ', System.StringSplitOptions.RemoveEmptyEntries).ToList();
+
+let getWordsFromFile (path: string): List<string> =
+    if File.Exists(path)
+    then
+        let words = new List<string>()
+        let sr = new StreamReader(path)
+        while (not sr.EndOfStream) do
+            let stringWords = getWordsFromString(cleanString(sr.ReadLine()))
+            words.AddRange(stringWords)
+        sr.Close()
+        words
+    else
+        new List<string>()
+
+let getWordLength(word: string): int =
+    word.Length
+
+let getLongestWord (words: List<string>) =
+    let uniqueWords = Array.distinct(words.ToArray())
+    let mapped = Array.map(fun word -> word, getWordLength(word)) uniqueWords
+    Array.maxBy (fun (_, length) -> length) mapped
+        
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+    let words = getWordsFromFile("/Users/imachiavelli/Downloads/TESTTEXT.txt")
+    let longestWord = getLongestWord(words)
+    match longestWord with
+    | (word, length) -> printf $"{word}: {length}"
+    0
